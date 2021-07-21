94c0d40abffb481bfe53b4d9d0331c11c0ccb4d4
leooskarpa <skarpaleo111@gmail.com>
2021-07-21 19:37:54 +0200

    Finished Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header.component';
+import Footer from './Components/Footer/Footer.component';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.component.js
@@ -0,0 +1,11 @@
+import './Footer.component.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            © TINEL Meetup 2020.
+        </div>
+    );
+}
+
+export default Footer;
