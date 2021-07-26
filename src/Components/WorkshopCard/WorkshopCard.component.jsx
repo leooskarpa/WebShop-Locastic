2235b144d6371c19e7f4b7a3cf55d1a5237c0903
Leo Škarpa <skarpaleo111@gmail.com>
2021-07-26 16:41:34 +0200

    Need to fix category icons

--- src/Components/WorkshopCard/WorkshopCard.component.jsx
+++ src/Components/WorkshopCard/WorkshopCard.component.jsx
@@ -5,23 +5,35 @@ import designIcon from '../../Images/brush.svg'
 import backendIcon from '../../Images/backend.svg'
 import frontendIcon from '../../Images/frontend.svg'
 import marketingIcon from '../../Images/marketing.svg'
-import { date } from 'check-types'
-
-
 
 
 const WorkshopCard = ({ workshop }) => {
     const date_and_time = new Date(workshop.date)
 
-    console.log(date_and_time)
-
     const date = `${date_and_time.getDate()}.${date_and_time.getMonth() + 1}.${date_and_time.getFullYear()}.`
     const time = `${date_and_time.getHours()}:${date_and_time.getMinutes()}`
+
+
+    const getCategory = (category) => {
+        switch (category) {
+            case 'design':
+                return <img className="workshopcard-category-icon" src={designIcon} alt="Design" />
+            case 'frontend':
+                return <img className="workshopcard-category-icon" src={frontendIcon} alt="Frontend" />
+            case 'backend':
+                return <img className="workshopcard-category-icon" src={backendIcon} alt="Backend" />
+            case 'marketing':
+                return <img className="workshopcard-category-icon" src={marketingIcon} alt="Marketing" />
+            default:
+                return <img className="workshopcard-category-icon" src={designIcon} alt="All" />
+        }
+    }
 
     return (
         <div className="workshopcard-container">
             <div className="workshopcard-img-container">
-                <img src={workshop.imageUrl} alt="Workshop" />
+                <img className="workshopcard-img" src={workshop.imageUrl} alt="Workshop" />
+                {getCategory(workshop.category)}
             </div>
             <div className="workshopcard-desc-container">
                 <div className="workshopcard-date-and-time">
